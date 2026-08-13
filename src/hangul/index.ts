35dabb570a59717d7d7f61185a9cc9b645186714
Seongho Bae <me@seonghobae.me>
2026-08-14 00:10:56 +0900

    feat(hangul): add bridge module boundary

---
+++ src/hangul/index.ts
@@ -0,0 +1,10 @@
+export async function openHangulDocument(): Promise<never> {
+  throw new Error('not implemented');
+}
+
+export async function exportHangulDocument(): Promise<never> {
+  throw new Error('not implemented');
+}
+
+export interface HangulDocumentEngine {}
+export interface HangulEngineDocument {}
